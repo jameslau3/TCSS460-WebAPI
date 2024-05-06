14723efa35e95b2447528bb9caeb27dac6c6417f
EvanAbrahamson27 <evanabrahamson27@gmail.com>
2024-05-06 14:33:08 -0700

    Added title queries and functionality

--- src/routes/open/books.ts
+++ src/routes/open/books.ts
@@ -5,18 +5,69 @@ import { pool, validationFunctions } from '../../core/utilities';
 
 const booksRouter: Router = express.Router();
 
-booksRouter.get('/books/all', (request: Request, response: Response) => {
+booksRouter.get('/all', (request: Request, response: Response) => {
     const theQuery = 'SELECT * FROM books';
 
     pool.query(theQuery)
         .then((result) => {
             response.send({
-                entries: result.rows
+                entries: result.rows,
             });
         })
         .catch((error) => {
             //log the error
             console.error('DB Query error on GET all');
+            console.error(error);
+            response.status(500).send({
+                message: 'server error - contact support',
+            });
+        });
+});
+
+booksRouter.get('/title/:title', (request: Request, response: Response) => {
+    const theQuery =
+        'SELECT title, authors, publication_year FROM books WHERE title ILIKE $1';
+    const values = ['%' + request.params.title + '%'];
+    pool.query(theQuery, values)
+        .then((result) => {
+            if (result.rowCount > 0) {
+                response.send({
+                    entries: result.rows,
+                });
+            } else {
+                response.status(404).send({
+                    message: `No books found with that title`,
+                });
+            }
+        })
+        .catch((error) => {
+            //log the error
+            console.error('DB Query error on GET title/~title');
+            console.error(error);
+            response.status(500).send({
+                message: 'server error - contact support',
+            });
+        });
+});
+
+booksRouter.get('/title/', (request: Request, response: Response) => {
+    const theQuery =
+        'SELECT title, authors, publication_year FROM books ORDER BY title asc';
+    pool.query(theQuery)
+        .then((result) => {
+            if (result.rowCount > 0) {
+                response.send({
+                    entries: result.rows,
+                });
+            } else {
+                response.status(404).send({
+                    message: `No books found in database`,
+                });
+            }
+        })
+        .catch((error) => {
+            //log the error
+            console.error('DB Query error on GET title/~title');
             console.error(error);
             response.status(500).send({
                 message: 'server error - contact support',
--- src/routes/open/index.ts
+++ src/routes/open/index.ts
@@ -1,9 +1,11 @@
 import express, { Router } from 'express';
 
 import { messageRouter } from './message';
+import { booksRouter } from './books';
 
 const openRoutes: Router = express.Router();
 
 openRoutes.use('/message', messageRouter);
+openRoutes.use('/books', booksRouter);
 
 export { openRoutes };
