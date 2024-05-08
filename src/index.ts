49a085896d277f2c42d0ea035dedd922512c68e7
44hassan <hassanabbas9146@gmail.com>
2024-05-08 14:32:26 -0700

    Update
    Books by author and rating.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response, request } from 'express';
 
 import cors from 'cors';
 
 import { routes } from './routes';
+import { pool } from './core/utilities';
 
 const app: Express = express();
 
@@ -19,6 +20,85 @@ app.use(routes);
 
 app.get('/', (request: Request, response: Response) => {
     response.send('<h1>Hello World!</h1>');
+});
+
+app.get('/books/all', (request: Request, response: Response) => {
+    const theQuery = 'SELECT * FROM books';
+
+    pool.query(theQuery)
+        .then((result) => {
+            response.send({
+                entries: result.rows,
+            });
+        })
+        .catch((error) => {
+            //log the error
+            console.error('DB Query error on GET all');
+            console.error(error);
+            response.status(500).send({
+                message: 'server error - contact support',
+            });
+        });
+});
+
+app.get('/books/by_author', (request: Request, response: Response) => {
+    const authorName = request.query.author;
+    if (!authorName) {
+        response.status(400).send({
+            message: 'Author name is required',
+        });
+        return;
+    }
+
+    const queryParams = [`%${authorName}%`];
+    const theQuery = 'SELECT * FROM books WHERE authors LIKE $1';
+
+    pool.query(theQuery, queryParams)
+        .then((result) => {
+            response.send({
+                entries: result.rows,
+            });
+        })
+        .catch((error) => {
+            //log the error
+            console.error('DB Query error on GET by author');
+            console.error(error);
+            response.status(500).send({
+                message: 'server error - contact support',
+            });
+        });
+});
+
+app.get('/books/by_rating', (request: Request, response: Response) => {
+    const ratingString = request.query.rating as string;
+
+    // Parse the rating string to a float
+    const rating = parseFloat(ratingString);
+
+    if (isNaN(rating) || rating < 1 || rating > 5) {
+        response.status(400).send({
+            message: 'Rating must be a number between 1 and 5',
+        });
+        return;
+    }
+
+    const queryParams = [rating];
+    const theQuery = 'SELECT * FROM books WHERE rating_avg = $1';
+
+    pool.query(theQuery, queryParams)
+        .then((result) => {
+            response.send({
+                entries: result.rows,
+            });
+        })
+        .catch((error) => {
+            //log the error
+            console.error('DB Query error on GET by rating');
+            console.error(error);
+            response.status(500).send({
+                message: 'server error - contact support',
+            });
+        });
 });
 
 app.listen(PORT, () => {
